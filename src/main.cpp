38d6d28d039560f17743f875e889863726ecc9b3
fbonc <felipe.bonchristiano@outlook.com>
2024-09-03 07:02:20 +0100

    glclearcolor

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,22 @@ int main() {
     }
 
     window = glfwCreateWindow(640, 480, "Frac", NULL, NULL);
-    
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate(); 
     return 0;
 }
