96fdd285e3e187da87202a3a36534cf62cdc814e
fbonc <felipe.bonchristiano@outlook.com>
2024-09-05 09:19:16 +0100

    bug fixing 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,51 @@
 #include "config.h"
+// #include <iostream>
+// #include <glad/glad.h>
+// #include <GLFW/glfw3.h>
 
-int main() {
-    GLFWwindow* window;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
-    if (!glfwInit()){
-        std::cout << "GLFW couldn't start" << std::endl;
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
-    window = glfwCreateWindow(640, 480, "Frac", NULL, NULL);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(800, 600, "ZMMR", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to open GLFW window" << std::endl;
+        return -1;
+    }
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        glfwTerminate();
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    while (!glfwWindowShouldClose(window)) {
-
-        glfwPollEvents();
-
-        glClear(GL_COLOR_BUFFER_BIT);
-
+    while(!glfwWindowShouldClose(window))
+    {
         glfwSwapBuffers(window);
+        glfwPollEvents();    
     }
 
-    glfwTerminate(); 
+    glfwTerminate();
     return 0;
 }
