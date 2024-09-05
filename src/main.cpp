a363a33be8c62ea90cf4bdaf918b7e2e7a87151d
fbonc <felipe.bonchristiano@outlook.com>
2024-09-05 09:22:21 +0100

    create black window

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window;
-    window = glfwCreateWindow(800, 600, "ZMMR", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "Frac", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to open GLFW window" << std::endl;
