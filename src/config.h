f74053efbc84135bd65b4572557354e8e6766f42
fbonc <felipe.bonchristiano@outlook.com>
2024-09-06 12:42:19 +0100

    main.cpp shader and shader modules functions

--- src/config.h
+++ src/config.h
@@ -7,3 +7,5 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+
+#include <vector>
--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,27 @@ int main() {
 }
 
 
+unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath) {
+
+	std::vector<unsigned int> modules;
+	modules.push_back(make_module(vertex_filepath, GL_VERTEX_SHADER));
+	modules.push_back(make_module(fragment_filepath, GL_FRAGMENT_SHADER));
+
+	unsigned int shader = glCreateProgram();
+	for (unsigned int shaderModule : modules) {
+		glAttachShader(shader, shaderModule);
+	}
+
+	glLinkProgram(shader);
+
+	int success;
+	glGetShaderiv(shader, GL_LINK_STATUS, &success);
+	if (!success) {
+		char errorLog[1024];
+		glGetProgramInfoLog(shader, 1024, NULL, errorLog);
+		std::cout << "Shader Linking error:\n" << errorLog << std::endl;
+}
+
 
 unsigned int make_module(const std::string& filepath, unsigned int module_type) {
 	std::ifstream file;
@@ -54,7 +75,13 @@ unsigned int make_module(const std::string& filepath, unsigned int module_type) 
 	glCompileShader(shaderModule);
 
 	int success;
-	glGetShaderiv(shaderModule, GL_COMPILE_STATUS, &status);
-	
+	glGetShaderiv(shaderModule, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		char errorLog[1024];
+		glGetShaderInfoLog(shaderModule, 1024, NULL, errorLog);
+		std::cout << "Shader module compilation error:\n" << errorLog << std::endl;
 
+	}
+
+	return shaderModule;
 }
