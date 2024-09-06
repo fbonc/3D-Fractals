dc06f065d187ee65a95cc20df9a40ad6f2a010f7
fbonc <felipe.bonchristiano@outlook.com>
2024-09-06 12:50:33 +0100

    make_shader and make_module functions completed

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,5 @@
 #include "config.h"
 
-
-int main() {
-	
-	GLFWwindow* window;
-
-	if (!glfwInit()) {
-		return -1;
-	}
-	
-	window = glfwCreateWindow(640, 480, "Fractalator", NULL, NULL);
-	glfwMakeContextCurrent(window);
-
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Couldn't load opengl" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-
-	glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
-	}
-
-	glfwTerminate();
-	return 0;
-}
-
-
-unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath) {
-
-	std::vector<unsigned int> modules;
-	modules.push_back(make_module(vertex_filepath, GL_VERTEX_SHADER));
-	modules.push_back(make_module(fragment_filepath, GL_FRAGMENT_SHADER));
-
-	unsigned int shader = glCreateProgram();
-	for (unsigned int shaderModule : modules) {
-		glAttachShader(shader, shaderModule);
-	}
-
-	glLinkProgram(shader);
-
-	int success;
-	glGetShaderiv(shader, GL_LINK_STATUS, &success);
-	if (!success) {
-		char errorLog[1024];
-		glGetProgramInfoLog(shader, 1024, NULL, errorLog);
-		std::cout << "Shader Linking error:\n" << errorLog << std::endl;
-}
-
-
 unsigned int make_module(const std::string& filepath, unsigned int module_type) {
 	std::ifstream file;
 	std::stringstream bufferedLines;
@@ -84,4 +29,74 @@ unsigned int make_module(const std::string& filepath, unsigned int module_type) 
 	}
 
 	return shaderModule;
+}
+
+
+unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath) {
+
+	std::vector<unsigned int> modules;
+	modules.push_back(make_module(vertex_filepath, GL_VERTEX_SHADER));
+	modules.push_back(make_module(fragment_filepath, GL_FRAGMENT_SHADER));
+
+	unsigned int shader = glCreateProgram();
+	for (unsigned int shaderModule : modules) {
+		glAttachShader(shader, shaderModule);
+	}
+
+	glLinkProgram(shader);
+
+	int success;
+	glGetShaderiv(shader, GL_LINK_STATUS, &success);
+	if (!success) {
+		char errorLog[1024];
+		glGetProgramInfoLog(shader, 1024, NULL, errorLog);
+		std::cout << "Shader Linking error:\n" << errorLog << std::endl;
+	}
+
+	for (unsigned int shaderModule : modules) {
+		glDeleteShader(shaderModule);
+	}
+
+	return shader;
+}
+
+
+
+
+int main() {
+	
+	GLFWwindow* window;
+
+	if (!glfwInit()) {
+		return -1;
+	}
+	
+	window = glfwCreateWindow(640, 480, "Fractalator", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Couldn't load opengl" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+
+	unsigned int shader = make_shader(
+		"../src/shaders/vertex.vert",
+		"../src/shaders/fragment.frag"
+	);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shader);
+		glfwSwapBuffers(window);
+	}
+
+
+	glDeleteProgram(shader);
+	glfwTerminate();
+	return 0;
 }
